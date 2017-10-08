5b6c501a8867c888e4f9f16b39f2dd20803623c8
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-08 11:12:18 +0100

    Goal Post - Initial Project Creation

---
+++ Projects/ios11-goalpost/Goal Post/Goal Post/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goal Post
+//
+//  Created by Thomas Pickup on 08/10/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import UIKit
+
+class vcMain: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
