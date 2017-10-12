4f83c391821d2570a9ba20596d2ac2ffa8afb4d4
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-12 22:18:26 +0100

    Created vcCreateGoal Storyboard and linked Outlets and Actions

--- Projects/ios11-goalpost/Goal Post/Goal Post/Controller/vcCreateGoal.swift
+++ Projects/ios11-goalpost/Goal Post/Goal Post/Controller/vcCreateGoal.swift
@@ -9,27 +9,35 @@
 import UIKit
 
 class vcCreateGoal: UIViewController {
-
+    // Outlets
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var btnShortTerm: UIButton!
+    @IBOutlet weak var btnLongTerm: UIButton!
+    @IBOutlet weak var btnNext: UIButton!
+    
+    // Variables
+    
+    // View Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    // Actions
+    @IBAction func onNextButtonPressed(_ sender: Any) {
+    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onShortTermButtonPressed(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onLongTermButtonPressed(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func onBackButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Functions
+    
 }
