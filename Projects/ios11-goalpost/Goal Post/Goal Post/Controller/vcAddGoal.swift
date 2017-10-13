d77a33f95babd9fc4f8f613486864108f79b47cd
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-13 21:08:36 +0100

    Layout and Outlets linked for vcAddGoal

---
+++ Projects/ios11-goalpost/Goal Post/Goal Post/Controller/vcAddGoal.swift
@@ -0,0 +1,32 @@
+//
+//  vcAddGoal.swift
+//  Goal Post
+//
+//  Created by Thomas Pickup on 13/10/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import UIKit
+
+class vcAddGoal: UIViewController {
+    // Outlets
+    @IBOutlet weak var txtProgress: UITextField!
+    @IBOutlet weak var btnCreateGoal: UIButton!
+    
+    // Variables
+    
+    // View Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // Actions
+    @IBAction func onCreateGoalPressed(_ sender: Any) {
+        
+    }
+    
+    // Functions
+    
+}
