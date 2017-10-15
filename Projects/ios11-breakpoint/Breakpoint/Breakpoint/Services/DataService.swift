206a2f3323cb9966b6784903540e5688d424f24a
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-15 20:47:14 +0100

    Set up public and private variables in DataService for DB References

--- Projects/ios11-breakpoint/Breakpoint/Breakpoint/Services/DataService.swift
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/Services/DataService.swift
@@ -7,9 +7,32 @@
 //
 
 import Foundation
+import Firebase
+
+// Database Constant
+let DB_BASE = Database.database().reference()
 
 class DataService {
+    // Variables
     static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
     
     
 }
