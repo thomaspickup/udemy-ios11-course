c8658db4c64eb61172faf8426e21a219dc9d0c1c
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-15 20:39:43 +0100

    Created Singleton DataService

---
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/Services/DataService.swift
@@ -0,0 +1,15 @@
+//
+//  DataService.swift
+//  Breakpoint
+//
+//  Created by Thomas Pickup on 15/10/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    
+}
