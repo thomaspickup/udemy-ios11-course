3bb9141d98c9f85c95bfa29becb5e6a43c0d58f7
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-17 21:12:03 +0100

    Created InSetTextField to set placeholder text colour and padding

---
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/View/InSetTextField.swift
@@ -0,0 +1,37 @@
+//
+//  InSetTextField.swift
+//  Breakpoint
+//
+//  Created by Thomas Pickup on 17/10/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import UIKit
+
+class InSetTextField: UITextField {
+    // Variables
+    private var padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    
+    // Functions
+    override func awakeFromNib() {
+        setUpView()
+        super.awakeFromNib()
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    func setUpView() {
+        let placeholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        self.attributedPlaceholder = placeholder
+    }
+}
