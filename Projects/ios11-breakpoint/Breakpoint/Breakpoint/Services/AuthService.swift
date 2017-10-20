424c499a3302a67fb032f48a22297d12669cfc49
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-20 21:56:55 +0100

    Created Base class for AuthService and initiated as Singleton

---
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/Services/AuthService.swift
@@ -0,0 +1,15 @@
+//
+//  AuthService.swift
+//  Breakpoint
+//
+//  Created by Thomas Pickup on 20/10/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+}
