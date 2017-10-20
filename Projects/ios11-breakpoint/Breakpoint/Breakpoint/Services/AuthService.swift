ad296ecc5620ab1153b3f220384351cf7da4d598
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-20 22:08:48 +0100

    Created User Registration Function

--- Projects/ios11-breakpoint/Breakpoint/Breakpoint/Services/AuthService.swift
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/Services/AuthService.swift
@@ -16,11 +16,21 @@ class AuthService {
     // Variables
     
     // Functions
-    func registerUser() {
-        
+    func registerUser(withEmail email: String, withPassword password: String, userCreationComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": user.email]
+            
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
     }
     
-    func loginUser() {
+    func loginUser(withEmail email: String, withPassword password: String, userLoginComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
         
     }
 }
