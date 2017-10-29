9ffa0ae089474586622a98bd00bbc62e66427d8a
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-29 10:25:02 +0000

    Created vcMe and added in outlets and actions to swift file

---
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/Controller/vcMe.swift
@@ -0,0 +1,29 @@
+//
+//  vcMe.swift
+//  Breakpoint
+//
+//  Created by Thomas Pickup on 29/10/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import UIKit
+
+class vcMe: UIViewController {
+    // Outlets
+    @IBOutlet weak var imgProfile: UIImageView!
+    @IBOutlet weak var lblUsers: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Variables
+    
+    // View Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Actions
+    @IBAction func onSignOutPressed(_ sender: Any) {
+    }
+    
+    // Functions
+}
