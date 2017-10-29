e04c39ed215910780ce285585ae32df8fc6295c4
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-29 11:57:52 +0000

    Created vcPost.swift and linked outlets and actions to swift file

---
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/Controller/vcPost.swift
@@ -0,0 +1,34 @@
+//
+//  vcPost.swift
+//  Breakpoint
+//
+//  Created by Thomas Pickup on 29/10/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import UIKit
+
+class vcPost: UIViewController {
+    // Outlets
+    @IBOutlet weak var imgProfile: UIImageView!
+    @IBOutlet weak var lblUser: UILabel!
+    @IBOutlet weak var txtMessage: UITextView!
+    
+    // Variables
+    
+    // View Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // Actions
+    @IBAction func onSendPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onExitPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    // Functions
+}
