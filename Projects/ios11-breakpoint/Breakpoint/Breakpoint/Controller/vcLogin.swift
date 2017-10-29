bf67d86042ecd0ad35e2e482ea94c5c7d54c1f13
Thomas Pickup <tpickup01@qub.ac.uk>
2017-10-29 09:46:54 +0000

    Added in actions to vcLogin and present function to display vcEmailSignIn

--- Projects/ios11-breakpoint/Breakpoint/Breakpoint/Controller/vcLogin.swift
+++ Projects/ios11-breakpoint/Breakpoint/Breakpoint/Controller/vcLogin.swift
@@ -9,27 +9,27 @@
 import UIKit
 
 class vcLogin: UIViewController {
-
+    // Outlets
+    
+    // Variables
+    
+    // View Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Actions
+    @IBAction func onEmailPressed(_ sender: Any) {
+        let vcEmailSignIn = storyboard?.instantiateViewController(withIdentifier: "vcEmailSignIn")
+        present(vcEmailSignIn!, animated: true, completion: nil)
+    }
+    @IBAction func onFacebookPressed(_ sender: Any) {
+    }
+    @IBAction func onGooglePressed(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Functions
+    
 }
